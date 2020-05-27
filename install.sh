2daec4d4ea4f893c22be43d64d31c9a0bed96b5d
c vw <dl1ycf@darc.de>
2020-05-27 14:45:24 +0200

    New install file that installs more homebrew packages

--- install.sh
+++ install.sh
@@ -2,4 +2,15 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+#
+# This installs all the "homebrew" packages needed for *running* pihpsdr.
+# Note the WDSP and SoapySDR libraries are contained in the app bundle
+#
 brew install gtk+3
+brew install pkg-config
+brew install portaudio
+brew install fftw
+#
+# This is for PrivacyProtection
+#
+brew analytics off 
